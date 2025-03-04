02ecb316264d6504eae00131f648fe488083fa71
Wohlstand <admin@wohlnet.ru>
2025-03-05 02:43:17 +0300

    Updated copyright year after a while

--- src/tiny_screenshoter.h
+++ src/tiny_screenshoter.h
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2018 Vitaly Novichkov
+ * Copyright (c) 2018-2025 Vitaly Novichkov
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
